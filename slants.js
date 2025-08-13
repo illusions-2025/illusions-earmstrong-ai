28087ff293202aa6fec858732c31858b79dd3d75
earmstrong-ai <earmstrong@scdsb.on.ca>
2025-08-13 10:05:48 -0400

    add circle

--- slants.js
+++ slants.js
@@ -37,7 +37,7 @@ function setup() {
 //if stillColour is truthy, image will be black or white
 //but not both (ie no illusion)
 function drawCircles(stillColour){
-ellipse(100,100, 100, 100);
+ellipse(100,100, 100, 100); 
 }
 
 //if stillColour is truthy, image will be black or white
