79efe57cc2f3447091d42266c974a625a9613a70
earmstrong-ai <earmstrong@scdsb.on.ca>
2025-08-13 10:14:11 -0400

    looping

--- slants.js
+++ slants.js
@@ -37,7 +37,10 @@ function setup() {
 //if stillColour is truthy, image will be black or white
 //but not both (ie no illusion)
 function drawCircles(stillColour){
-ellipse(100,100, 100, 100); 
+    for (x=1, x < 100, x++)
+    {
+    ellipse(x,100, 100, 100); 
+    }
 }
 
 //if stillColour is truthy, image will be black or white
